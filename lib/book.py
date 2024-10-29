49466e5e76a7cbc3dac8b1213aeb65cb3af666fb
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-10-29 21:47:08 +0300

    complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        if not isinstance(title, str) or len(title) < 1 or len(title) > 25:
+            print("Title must be string between 1 and 25 characters.")
+            return
+        self.title = title
+        self.page_count = page_count  # Call the setter
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        if not isinstance(brand, str) or len(brand) < 1 or len(brand) > 25:
+            print("Brand must be string between 1 and 25 characters.")
+            return
+        self.brand = brand
+        self.size = size  # Call the setter
+        self.condition = 'New'  # Default condition when initialized
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")  # Update the message here
